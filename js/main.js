2e0e7d89ac5f4cafb6e8235b1ac39e06c8adac97
Gerardo Sorto <gerardo.sorto31@outlook.com>
2020-03-13 12:23:21 -0600

    Uploading Files

---
+++ js/main.js
@@ -0,0 +1,21 @@
+var map;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -34.397, lng: 150.644},
+          zoom: 8
+        });
+      }
+
+//Smooth Scrolling
+$('header a, .btn').on('click', function(event){
+  if(this.hash !== ''){
+    event.preventDefault();
+    const hash = this.hash;
+    $('html, body').animate(
+      {
+      scrollTop: $(hash).offset().top - 100
+    },
+    800
+    );
+  }
+});
